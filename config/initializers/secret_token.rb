d2df47780e179c83240a76531a13051dcf5ea1c2
CruiseWise <cruisewise@malta.local>
2012-04-03 15:37:35 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NicoTest::Application.config.secret_token = '2eb623e4d96bd62bba1dfa1beb81f7ebc12b077da36a1b7a6b43ef0e88b0291bbeae15bc31fbcffa9268411989b061b35fa94498a26149f55ae3dd4b884c9e0a'
